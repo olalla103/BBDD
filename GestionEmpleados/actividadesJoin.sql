162dd0ea174dfc18ef3a4d3b14d8e1ce97ab41c4
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-03-15 12:35:48 +0100

    15 de marzo

--- GestionEmpleados/actividadesJoin.sql
+++ GestionEmpleados/actividadesJoin.sql
@@ -1,45 +1,177 @@
 -- Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno.
-select e.nombre, d.* from empleado e join departamento d on empleado
+select
+    e.nombre,
+    d.*
+from
+    empleado e
+    join departamento d on e.id_departamento = d.id;
+
 -- Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno.
 -- Ordena el resultado, en primer lugar por el nombre del departamento (en orden alfabético) y 
 --en segundo lugar por los apellidos y el nombre de los empleados.
+select
+    e.nombre,
+    e.id,
+    d.*
+from
+    empleado e
+    join departamento d on e.id_departamento = d.id
+order by
+    d.nombre asc,
+    e.apellido1 asc,
+    e.apellido2 asc,
+    e.nombre;
 
 -- Devuelve un listado con el identificador y el nombre del departamento, solamente de aquellos
 -- departamentos que tienen empleados.
+select
+    distinct(d.id),
+    d.nombre
+from
+    departamento d
+    left join empleado e on e.id_departamento = d.id
+where
+    e.id_departamento is not null;
 
 -- Devuelve un listado con el identificador, el nombre del departamento y el valor del presupuesto
 -- actual del que dispone, solamente de aquellos departamentos que tienen empleados.
--- El valor del presupuesto actual lo puede calcular restando al valor del presupuesto inicial
--- (columna presupuesto) el valor de los gastos que ha generado (columna gastos).
+select
+    distinct(d.id),
+    d.nombre,
+    d.presupuesto - d.gastos as 'presupuesto actual'
+from
+    departamento d
+    left join empleado e on d.id = e.id_departamento
+where
+    e.id_departamento is not null;
 
 -- Devuelve el nombre del departamento donde trabaja el empleado que tiene el nif 38382980M.
+select
+    d.nombre
+from
+    departamento d
+    join empleado e on e.id_departamento = d.id
+where
+    e.nif = '38382980M';
 
 -- Devuelve el nombre del departamento donde trabaja el empleado Pepe Ruiz Santana.
+select
+    d.nombre
+from
+    departamento d
+    join empleado e on e.id_departamento = d.id
+where
+    e.nombre = 'Pepe'
+    and e.apellido1 = 'Ruiz'
+    and e.apellido2 = 'Santana';
 
 -- Devuelve un listado con los datos de los empleados que trabajan en el departamento de I+D.
 -- Ordena el resultado alfabéticamente.
+select
+    e.*
+from
+    departamento d
+    join empleado e on e.id_departamento = d.id
+where
+    d.nombre = 'I+D'
+order by
+    d.nombre asc;
 
 -- Devuelve un listado con los datos de los empleados que trabajan en el departamento de Sistemas,
 -- Contabilidad o I+D. Ordena el resultado alfabéticamente.
+select
+    e.*
+from
+    departamento d
+    join empleado e on e.id_departamento = d.id
+where
+    d.nombre = 'I+D'
+    or d.nombre = 'Sistemas'
+    or d.nombre = 'Contabilidad'
+order by
+    e.id;
 
 -- Devuelve una lista con el nombre de los empleados que tienen los departamentos que no tienen un
 -- presupuesto entre 100000 y 200000 euros.
+select
+    e.nombre
+from
+    empleado e
+    join departamento d on e.id_departamento = d.id
+where
+    d.presupuesto not between 100000
+    and 200000;
 
 -- Devuelve un listado con el nombre de los departamentos donde existe algún empleado cuyo segundo
 -- apellido sea NULL. Tenga en cuenta que no debe mostrar nombres de departamentos que estén repetidos.
+select
+    distinct(d.nombre)
+from
+    empleado e
+    right join departamento d on e.id_departamento = d.id
+where
+    e.apellido2 is null;
 
 -- con left y right join
 -- Devuelve un listado con todos los empleados junto con los datos de los departamentos donde trabajan.
 -- Este listado también debe incluir los empleados que no tienen ningún departamento asociado.
+select
+    e.*,
+    d.*
+from
+    empleado e
+    left join departamento d on e.id_departamento = d.id;
 
 -- Devuelve un listado donde sólo aparezcan aquellos empleados que no tienen ningún departamento asociado.
+select
+    e.*
+from
+    empleado e
+    left join departamento d on e.id_departamento = d.id
+where
+    e.id_departamento is null;
 
 -- Devuelve un listado donde sólo aparezcan aquellos departamentos que no tienen ningún empleado asociado.
+select
+    d.*
+from
+    empleado e
+    right join departamento d on e.id_departamento = d.id
+where
+    e.id_departamento is null;
 
 -- Devuelve un listado con todos los empleados junto con los datos de los departamentos donde trabajan.
 -- El listado debe incluir los empleados que no tienen ningún departamento asociado y los departamentos
 -- que no tienen ningún empleado asociado. Ordene el listado alfabéticamente por el nombre del departamento.
 
+    select
+        *
+    from
+        empleado e
+        left join departamento d on e.id_departamento = d.id
+    where
+        d.id is null
+
+union
+(
+    select
+        *
+    from
+        empleado em
+        right join departamento de on em.id_departamento = de.id
+    where
+        em.id_departamento is null
+    order by
+        de.nombre
+);
+
 -- Devuelve un listado con los empleados que no tienen ningún departamento asociado y los departamentos
 -- que no tienen ningún empleado asociado. Ordene el listado alfabéticamente por el nombre del departamento.
-
+select
+    e.*,
+    d.*
+from
+    empleado e
+    right join departamento d on e.id_departamento = d.id
+order by
+    d.nombre;
---
+++ GestionVentas/actividadesJoin.sql
@@ -0,0 +1,17 @@
+--Devuelve un listado con el identificador, nombre y los apellidos de todos los clientes que han realizado algún pedido. El listado debe estar ordenado alfabéticamente y se deben eliminar los elementos repetidos.
+--Devuelve un listado que muestre todos los pedidos que ha realizado cada cliente. El resultado debe mostrar todos los datos de los pedidos y del cliente. El listado debe mostrar los datos de los clientes ordenados alfabéticamente.
+--Devuelve un listado que muestre todos los pedidos en los que ha participado un comercial. El resultado debe mostrar todos los datos de los pedidos y de los comerciales. El listado debe mostrar los datos de los comerciales ordenados alfabéticamente.
+--Devuelve un listado que muestre todos los clientes, con todos los pedidos que han realizado y con los datos de los comerciales asociados a cada pedido.
+--Devuelve un listado de todos los clientes que realizaron un pedido durante el año 2017, cuya cantidad esté entre 300 € y 1000 €.
+--Devuelve el nombre y los apellidos de todos los comerciales que han participado en algún pedido realizado por María Santana Moreno.
+--Devuelve el nombre de todos los clientes que han realizado algún pedido con el comercial Daniel Sáez Vega.
+
+--Composición externa
+--Resuelva todas las consultas utilizando las cláusulas LEFT JOIN y RIGHT JOIN.
+
+--Devuelve un listado con todos los clientes junto con los datos de los pedidos que han realizado. Este listado también debe incluir los clientes que no han realizado ningún pedido. El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los clientes.
+--Devuelve un listado con todos los comerciales junto con los datos de los pedidos que han realizado. Este listado también debe incluir los comerciales que no han realizado ningún pedido. El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los comerciales.
+--Devuelve un listado que solamente muestre los clientes que no han realizado ningún pedido.
+--Devuelve un listado que solamente muestre los comerciales que no han realizado ningún pedido.
+--Devuelve un listado con los clientes que no han realizado ningún pedido y de los comerciales que no han participado en ningún pedido. Ordene el listado alfabéticamente por los apellidos y el nombre. En el listado deberá diferenciar de algún modo los clientes y los comerciales.
+
